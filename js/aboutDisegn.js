c815571382b8e57e41a915ca3d285dd6ea02768c
Carlos Uribe <carlos.uribec@upb.edu.co>
2021-02-02 08:47:58 -0500

    Cuadrando el About sin exito

--- js/aboutDisegn.js
+++ js/aboutDisegn.js
@@ -6,10 +6,16 @@ var contenedorAcerca = document.querySelector(".contenedorAcerca");
 var contenedorAcerca2 = document.querySelector(".contenedorAcerca2");
 
 function infoPag(){
-    contenedorAcerca2.style.display = "flex";
+    console.log("Boton execute");
+    contenedorAcerca.style.opacity = 0;
     contenedorAcerca.style.display = "none";
+    contenedorAcerca2.style.display = "flex";
+    contenedorAcerca2.style.opacity = 1;
+    
 }
 function infoYo(){
+    contenedorAcerca2.style.opacity = 0;
     contenedorAcerca2.style.display = "none";
     contenedorAcerca.style.display = "flex";
+    contenedorAcerca.style.opacity = 1;
 }
